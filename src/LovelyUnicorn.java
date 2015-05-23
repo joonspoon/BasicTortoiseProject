68fc0aee6d2f46e29e41cf248493a5187d7651de
June Clarke <The League of Amazing Programmers>
2015-05-23 13:34:22 -0700

    testing new TKP configuration

---
+++ src/LovelyUnicorn.java
@@ -0,0 +1,11 @@
+import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.logo.Turtle.Animals;
+
+public class LovelyUnicorn {
+
+	public static void main(String[] args) {
+		Tortoise.setAnimal(Animals.Unicorn);
+		Tortoise.show();
+	}
+
+}
