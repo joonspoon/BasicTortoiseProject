b8828b42fadb1346c3b8e44327b7d49a7c07d8cb
June Clarke <The League of Amazing Programmers>
2015-08-05 11:00:17 -0700

    things to be ignored

--- src/DeathKnell.java
+++ src/DeathKnell.java
@@ -7,30 +7,12 @@ import javax.sound.sampled.Clip;
 
 public class DeathKnell {
 	public static void main(String[] args) {
-		//playDeathKnell();
-		Scanner s = new Scanner(System.in);
-		int x = 0;
-		//x = s.nextInt();
-		System.out.println(s);
-		/*int last = 0;
-		int current = 1;
-		int next = 0;
-		
-		for(int i = 0 ; i < 10; i++)
-		{
-			next = current + last;
-			last = current;
-			current = next;
-			
-			System.out.println(next);
-			
-			
-		}*/
+		new DeathKnell().playDeathKnell();
 	}
 
-	public static void playDeathKnell() {
+	public void playDeathKnell() {
 		try {
-			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("/Users/Guest/LeagueStuff/league-sounds/funeral-march.wav"));
+			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("/Users/leagueleadteacher/Google Drive/league-sounds/funeral-march.wav"));
 			Clip clip = AudioSystem.getClip();
 			clip.open(audioInputStream);
 			clip.start();
